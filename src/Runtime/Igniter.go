3612871c571e1378d4005ed021d368a3b211fed8
antegral <antegral@antegral.net>
2023-02-20 21:57:13 +0900

    Add: Igniter, ModuleManager

--- src/Runtime/Igniter.go
+++ src/Runtime/Igniter.go
@@ -1,1 +1,21 @@
 package runtime
+
+import (
+	"flag"
+
+	"antegral.net/chatanium/src/Runtime/Log"
+)
+
+var LOGMODE int
+
+func Ignite() {
+	InitLog()
+}
+
+func InitLog() {
+	flag.IntVar(&LOGMODE, "logging-mode", 3, "Logging mode")
+	flag.Parse()
+
+	Log.Init(LOGMODE)
+	Log.Info.Println("Starting Chatanium...")
+}
--- src/Runtime/ModuleManager.go
+++ src/Runtime/ModuleManager.go
@@ -1,1 +1,49 @@
 package runtime
+
+import (
+	Backends "antegral.net/chatanium/src/Backend"
+	"antegral.net/chatanium/src/Runtime/Interface"
+	"antegral.net/chatanium/src/Runtime/Log"
+)
+
+func CallModule(Module *ChataniumModule) {
+	if err := Module.OnInit(); err != nil {
+		Log.Error.Printf("Runtime > Some Module occurred Error during OnInit()")
+		Log.Error.Fatalln(err)
+	}
+
+	if err := Module.OnStart(); err != nil {
+		Info := Module.GetInfo()
+		Log.Error.Printf(Info.Name, " > Error occurred during OnStart()")
+		Log.Error.Fatalln(err)
+	}
+
+	// TODO: 모듈과 런타임간 통신을 위해 모듈에 대한 채널 반환
+}
+
+func GetBackend(BackendType string, Module Interface.ModuleInfo) *Interface.ChataniumBackend {
+	Log.Info.Print(Module.Name, " > Access granted to backend")
+	switch BackendType {
+	case "discord":
+		return &Backends.Discord{}
+		break
+	default:
+		Log.Error.Fatal(Module.Name, " > Unknown backend: ", BackendType)
+		return nil
+		break
+	}
+}
+
+type ChataniumModule struct {
+	Info Interface.ModuleInfo
+}
+
+func (t *ChataniumModule) OnInit() error
+
+func (t *ChataniumModule) OnStart() error
+
+func (t *ChataniumModule) GetInfo() *Interface.ModuleInfo
+
+func (t *ChataniumModule) GetBackend(Backend Interface.ChataniumBackend) error
+
+func (t *ChataniumModule) OnMessage(Request string) Interface.ModuleResponse
