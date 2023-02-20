10faf39a765f50970ac140d16d0a2dc0941d1289
antegral <antegral@antegral.net>
2023-02-20 21:57:36 +0900

    Change: using Igniter

--- src/main.go
+++ src/main.go
@@ -1,24 +1,9 @@
 package chatanium
 
 import (
-	"flag"
-
-	"antegral.net/chatanium/src/Runtime/Log"
+	Runtime "antegral.net/chatanium/src/Runtime"
 )
 
-var LoggingMode int
-
 func main() {
-	Log.Init()
-}
-
-func InitLog() {
-	CheckFlags()
-	Log.Init(LoggingMode)
-	Log.Info.Println("Starting Chatanium...")
-}
-
-func CheckFlags() {
-	flag.IntVar(&LoggingMode, "logging-mode", 3, "Logging mode")
-	flag.Parse()
+	Runtime.Ignite()
 }
