04c330eb15c43fcafab54b434daba8a44103ed1a
antegral <antegral@antegral.net>
2023-02-25 22:54:20 +0900

    Add: GetEnv()

---
+++ src/Runtime/Env.go
@@ -0,0 +1,16 @@
+package runtime
+
+import "github.com/joho/godotenv"
+
+func GetEnv(FilePath string) (error, string) {
+	var myEnv map[string]string
+	myEnv, err := godotenv.Read(FilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, v := range myEnv {
+	}
+
+	s3Bucket := myEnv[""]
+}
