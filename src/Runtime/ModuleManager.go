e894f8c448c5cdb5df960144f80b4c2a14f850d8
antegral <antegral@antegral.net>
2023-02-25 22:53:56 +0900

    Change: use uppercase

--- src/Runtime/ModuleManager.go
+++ src/Runtime/ModuleManager.go
@@ -28,15 +28,15 @@ func GetBackend(BackendType string, Module IChatanium.ModuleInfo) IChatanium.Bac
 
 	switch BackendType {
 	case "discord":
-		backend := Backends.Discord{}
-		backend.Init(Module)
+		Backend := Backends.Discord{}
+		Backend.Init(Module)
 		// TODO: 모듈에서 Credential을 알 수 없게 privately하게 짜기
 		// DotEnv는 모듈측에서 os.Getenv로 읽을 수 있으므로 사용 할 수 없음.
 		// 1. 경로 방식으로 privately하게 env 파일을 가져오고 로컬에서만 쓰면 되지 않을까?
 		// 1-1. 프로그램 실행 인수로 암호화된 env 파일의 key를 가져오게?
-		backend.SetCredentials(os.Getenv())
-		backend.Connect()
-		return &backend
+		Backend.SetCredentials(os.Getenv())
+		Backend.Connect()
+		return &Backend
 		break
 	default:
 		break
