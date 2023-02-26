cf7609285f3949f8bc761dd87fe278356027340e
antegral <antegral@antegral.net>
2023-02-26 18:52:34 +0900

    Remove: GetEnv()

--- src/Runtime/Env.go
+++ src/Runtime/Env.go
@@ -1,16 +1,16 @@
 package runtime
 
-import "github.com/joho/godotenv"
+// import "github.com/joho/godotenv"
 
-func GetEnv(FilePath string) (error, string) {
-	var myEnv map[string]string
-	myEnv, err := godotenv.Read(FilePath)
-	if err != nil {
-		return nil, err
-	}
+// func GetEnv(FilePath string) (error, string) {
+// 	var myEnv map[string]string
+// 	myEnv, err := godotenv.Read(FilePath)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	for _, v := range myEnv {
-	}
+// 	for _, v := range myEnv {
+// 	}
 
-	s3Bucket := myEnv[""]
-}
+// 	s3Bucket := myEnv[""]
+// }
