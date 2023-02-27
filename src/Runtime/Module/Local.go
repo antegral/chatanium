be049ab5a76bc2d213f7ac484d9f1fff0d34f992
antegral <antegral@antegral.net>
2023-02-27 20:55:04 +0900

    Add: Log Point (Verbose)

--- src/Runtime/Module/Local.go
+++ src/Runtime/Module/Local.go
@@ -8,13 +8,17 @@ import (
 	"antegral.net/chatanium/src/Runtime/Log"
 )
 
-func GetModule(FilePath string) IChatanium.Module {
+func Get(FilePath string) IChatanium.Module {
+	Log.Verbose.Printf("Getting module: %s", FilePath)
+
 	File, err := os.Stat(FilePath)
 	if err == nil {
 		Log.Error.Printf("%s > Module import failure", File.Name())
 		Log.Error.Fatalln(err)
 		return nil
 	}
+
+	Log.Verbose.Printf("Valid file. Opening plugin...")
 
 	ModulePlugin, err := plugin.Open(FilePath)
 	if err != nil {
@@ -23,23 +27,30 @@ func GetModule(FilePath string) IChatanium.Module {
 		return nil
 	}
 
-	ModuleSymbols, err := ModulePlugin.Lookup("ChataniumModule")
+	Log.Verbose.Printf("Looking up module symbols...")
+
+	ModuleSymbol, err := ModulePlugin.Lookup("ChataniumModule")
 	if err != nil {
 		Log.Error.Printf("%s > ", File.Name())
 		Log.Error.Fatalln(err)
 		return nil
 	}
 
-	ChataniumModule, ok := ModuleSymbols.(IChatanium.Module)
+	Log.Verbose.Printf("Type-Casting Module...")
+
+	ChataniumModule, ok := ModuleSymbol.(IChatanium.Module)
 	if !ok {
-		Log.Error.Printf("%s > Module Type Assertion failure", File.Name())
+		Log.Error.Fatalf("%s > Module Type-Casting failure", File.Name())
 		return nil
 	}
+
+	Log.Verbose.Printf("Successfully converted to module.")
 
 	return ChataniumModule
 }
 
-func StartModule(Module IChatanium.Module) IChatanium.Module {
+func Start(Module IChatanium.Module) IChatanium.Module {
+	Log.Verbose.Printf("Starting module: %s", Module.GetInfo().Name)
 	if err := Module.OnInit(); err != nil {
 		Log.Error.Printf("%s > OnInit() failure")
 		Log.Error.Fatalln(err)
@@ -48,7 +59,7 @@ func StartModule(Module IChatanium.Module) IChatanium.Module {
 
 	if err := Module.OnStart(); err != nil {
 		Info := Module.GetInfo()
-		Log.Error.Printf(Info.Name, " > Error occurred during OnStart()")
+		Log.Error.Printf(Info.Name, " > OnStart() failure")
 		Log.Error.Fatalln(err)
 		return nil
 	}
